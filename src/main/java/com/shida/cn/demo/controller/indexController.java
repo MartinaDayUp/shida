72304c1464ddfb7ca98d18e3e0a611fdccf5c7a1
MartinaDayUp <1963615455@qq.com>
2022-05-15 14:39:40 +0800

    输入用户名自动查找用户是否存在且显示

--- src/main/java/com/shida/cn/demo/controller/indexController.java
+++ src/main/java/com/shida/cn/demo/controller/indexController.java
@@ -1,11 +1,15 @@
 package com.shida.cn.demo.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.shida.cn.demo.entity.User;
 import com.shida.cn.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author 123
@@ -20,5 +24,18 @@ public class indexController {
         userService.addUser(user);
 
         return "login.jsp";
+    }
+
+    @RequestMapping("/ajaxUser")
+    @ResponseBody
+    public JSONObject getAjax(@RequestParam String username){
+        JSONObject jsonObject = new JSONObject();
+        User user= userService.findUser(username);
+        if(user!=null){
+            jsonObject.put("userExit",true);
+        }else {
+            jsonObject.put("userExit",false);
+        }
+        return jsonObject;
     }
 }
